93efabf8328141e500e98184f1ecb01fd20c84de
victor <victorreisprog@gmail.com>
2023-11-05 22:52:08 -0300

    top k frequent elements

---
+++ arrays_and_hashing/347.Top_K_Frequent_Elements.go
@@ -0,0 +1,30 @@
+func topKFrequent(nums []int, k int) []int {
+	hash := make(map[int]int)
+
+	for _, val := range nums {
+		hash[val]++
+	}
+
+	bucket := make([][]int, len(nums)+1)
+	for number, frequency := range hash {
+		// colocando no array de array bucket a partir do count -> values
+		// index => frequencia
+		// dnetro do array => number
+		bucket[frequency] = append(bucket[frequency], number)
+	}
+
+	result := make([]int, 0)
+	// como nosso bucket está armazenando os elementos de menor ocorrencia
+	// para os de maior ocorrencia, podemos iterar do ultimo elemento do bucket
+	// até o primeiro elemento do bucket, inserindo dentro de um array
+	// após fazer isso podemos retornar um slice com quantidade de elementos
+	// que queremos
+	for i := len(bucket) - 1; i > 0 && k > 0; i-- {
+		if len(bucket[i]) != 0 {
+			result = append(result, bucket[i]...)
+		}
+	}
+
+	return result[:k]
+
+}
