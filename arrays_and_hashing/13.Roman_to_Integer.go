a8977a25e03fee61d64f4ab872305f8efc89dede
victor <victorreisprog@gmail.com>
2023-11-07 07:38:32 -0300

    converting roman numbers to integer

---
+++ arrays_and_hashing/13.Roman_to_Integer.go
@@ -0,0 +1,30 @@
+// CONVERTER NÚMEROS ROMANOS ( STRING ) PARA INTEIROS
+
+func romanToInt(s string) int {
+	roman := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
+
+	count := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		currStr := string(s[i])
+		if i < len(s)-1 {
+			if roman[currStr] < roman[string(s[i+1])] {
+				count -= roman[currStr]
+			} else {
+				count += roman[currStr]
+			}
+		} else {
+			count += roman[currStr]
+		}
+	}
+
+	return count
+
+}
